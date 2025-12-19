bde606ccde13f4a534539ef6cfd6454839bd7803
saimaali-tech <saimaalisaima313@gmail.com>
2025-12-19 13:29:21 +0500

    changed the content of node file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello from Node.js 🚀</h1><p>This is a simple Node app.</p>");
+  res.end("hello its a simple node.js app");
 });
 
 server.listen(3000, () => {
